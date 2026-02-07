56fc6d0dc9ccb6a8022866c75b3c290aed8a4b87
parshant <ahlawatparshant57@gmail.com>
2026-02-07 16:26:39 +0530

    ignore ide and build files

---
+++ src/basics/InputOutput.java
@@ -0,0 +1,13 @@
+package basics;
+
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your age : ");
+        int a = sc.nextInt();
+        System.out.println("Your age is " +  a);
+
+    }
+}
